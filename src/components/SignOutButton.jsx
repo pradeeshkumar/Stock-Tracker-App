20bff0c797667d18f1d6843383df522404e9016d
pradeeshkumar <31466382+pradeeshkumar@users.noreply.github.com>
2022-04-27 16:58:38 +0000

    logo

--- src/components/SignOutButton.jsx
+++ src/components/SignOutButton.jsx
@@ -3,6 +3,8 @@ import { useMsal } from "@azure/msal-react";
 import { logoutRequest } from "../authConfig";
 import Button from "react-bootstrap/Button";
 import Navbar from "react-bootstrap/Navbar";
+import {Container} from 'react-bootstrap';
+
 
 
 
@@ -19,8 +21,11 @@ export const SignOutButton = () => {
     const { instance } = useMsal();
 
     return (
-        <Navbar className="bg-primary mb-5">
-            <Button variant="secondary" className="mr-auto" onClick={() => signOutClickHandler(instance)}>Sign Out</Button>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#">STA</Navbar.Brand>
+                <Button variant="secondary" className="mr-auto" onClick={() => signOutClickHandler(instance)}>Sign Out</Button>
+            </Container>
         </Navbar>
     );
 }
