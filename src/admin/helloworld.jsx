997f3728a9de25f438a4f475b66030f951984209
pradeeshkumar <31466382+pradeeshkumar@users.noreply.github.com>
2022-04-28 05:13:30 +0000

    c

--- src/admin/helloworld.jsx
+++ src/admin/helloworld.jsx
@@ -11,6 +11,7 @@ export const helloworld = (props) => {
 export async function gethelloworld() {
     const res = await fetch("https://dainty-licorice-4d88aa.netlify.app/.netlify/functions/hello");
     const result = await res.json;
+    console.log(result);
     return {
         props: {
             message: result.message
