6fdfa0cea9e622e628928cc57c16c750f824f71c
pradeeshkumar <31466382+pradeeshkumar@users.noreply.github.com>
2022-04-28 04:58:53 +0000

    helloworld

---
+++ src/admin/helloworld.jsx
@@ -0,0 +1,19 @@
+export const helloworld = (props) => {
+    return (
+        <div>
+            {props.message}
+        </div>
+    )
+};
+
+export async function gethelloworld() {
+    const res = await fetch("https://dainty-licorice-4d88aa.netlify.app/.netlify/functions/hello");
+    const result = await res.json;
+    return {
+        props: {
+            message: result.message
+        }
+    }
+}
+
+export default helloworld;
--- src/components/MainTab.jsx
+++ src/components/MainTab.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
+import '../admin/helloworld';
 export const MainTab = () => {
   return (
     <Tabs defaultActiveKey="Details" id="uncontrolled-tab-example" className="mb-3">
         <Tab eventKey="Details" title="Details">
+          <helloworld />
         </Tab>
         <Tab eventKey="Preferences" title="Preferences">
         </Tab>
